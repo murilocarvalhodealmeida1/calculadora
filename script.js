a1c3ac90185e6f9777f79a0844f9b6901c45b7f4
murilocarvalhodealmeida1 <159334281+murilocarvalhodealmeida1@users.noreply.github.com>
2024-09-16 09:38:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function bask(){
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    let c = parseFloat(document.getElementById('c').value);
+    let delta = (b*b)-(4*a*c);
+    let rdelta = Math.sqrt (delta);
 
+    if (delta == 0){
+        alert('sem resolução quando delta é igual a 0');
+    }else if (delta < 0){
+        let x1 = (-b)/(2*a);
+        document.getElementById('res').innerText = 'Resultado: ' + x1;
+    }else{
+        let x1 = (-b+rdelta)/(2*a);
+        let x2 = (-b-rdelta)/(2*a);
+        document.getElementById('res').innerText = 'Resultado: ' + x1 + ' e ' + x2;
+    }
+}
